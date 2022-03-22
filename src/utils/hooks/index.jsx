31d7a2c9b20544e31d02201bd1b366f2954641c9
Yada Ly <lyyada@gmail.com>
2022-03-22 14:12:49 +0100

    rem themecontext

--- src/utils/hooks/index.jsx
+++ src/utils/hooks/index.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect, useContext } from 'react'
-import { ThemeContext } from '../context'
+import { useState, useEffect } from 'react'
 
 export function useFetch(url) {
   const [data, setData] = useState({})
