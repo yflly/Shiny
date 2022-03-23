d7144dc7637ec4d7da4159a9ce8bd0ebca5a0f56
Yada Ly <lyyada@gmail.com>
2022-03-23 11:41:04 +0100

    correct file

--- src/features/survey.js
+++ src/features/survey.js
@@ -1,26 +1,65 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { selectSurvey } from '../utils/selectors'
 
-const themeSlice = createSlice({
-  // le nom du slice
-  name: 'theme',
-  // le state initial
-  initialState: 'light',
-  // reducers permet de définir les actions et le reducer
+const initialState = {
+  status: 'void',
+  data: null,
+  error: null,
+}
+
+export async function fetchOrUpdateSurvey(dispatch, getState) {
+  const status = selectSurvey(getState()).status
+  if (status === 'pending' || status === 'updating') {
+    return
+  }
+  dispatch(actions.fetching())
+  try {
+    const response = await fetch('http://localhost:8000/survey')
+    const data = await response.json()
+    dispatch(actions.resolved(data))
+  } catch (error) {
+    dispatch(actions.rejected(error))
+  }
+}
+
+const { actions, reducer } = createSlice({
+  name: 'survey',
+  initialState,
   reducers: {
-    // l'action toggle ('theme/toggle')
-    toggle: (state) => {
-      return state === 'light' ? 'dark' : 'light'
+    fetching: (draft, action) => {
+      if (draft.status === 'void') {
+        draft.status = 'pending'
+        return
+      }
+      if (draft.status === 'rejected') {
+        draft.error = null
+        draft.status = 'pending'
+        return
+      }
+      if (draft.status === 'resolved') {
+        draft.status = 'updating'
+        return
+      }
+      return
     },
-    // l'action set ('theme/set')
-    set: (state, action) => {
-      return action.payload
+    resolved: (draft, action) => {
+      if (draft.status === 'pending' || draft.status === 'updating') {
+        draft.data = action.payload
+        draft.status = 'resolved'
+        return
+      }
+      return
+    },
+    rejected: (draft, action) => {
+      if (draft.status === 'pending' || draft.status === 'updating') {
+        draft.error = action.payload
+        draft.data = null
+        draft.status = 'rejected'
+        return
+      }
+      return
     },
   },
 })
 
-// on extrait les actions et le reducer
-const { actions, reducer } = themeSlice
-// on export chaque action individuellement
-export const { set, toggle } = actions
-// on export le reducer comme default export
 export default reducer
